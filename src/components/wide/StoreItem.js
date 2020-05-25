d8cdb6e6496d02a95cdc7d3bddbac6635428ddbb
AlexandreLeonetti <al.leo@wanadoo.fr>
2020-05-25 16:17:39 +0800

    1

---
+++ src/components/wide/StoreItem.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './StoreItem.css';
+
+
+class StoreItem extends Component {
+	constructor(props){
+		super(props)
+	}
+	render(){
+		return(<div>
+			<div className="StoreItem">
+				store item
+			</div>
+		</div>
+		);
+
+
+	}
+
+}
+
+export default StoreItem;
